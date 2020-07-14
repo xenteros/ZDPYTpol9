34ba2ce06555b1ba98a5ea70710fcda4a222e499
xenteros <adan@gurgul.com.pl>
2020-07-14 20:05:29 +0200

    Add code for spliting regex

--- 302-custom-regex-parser.py
+++ 302-custom-regex-parser.py
@@ -33,9 +33,42 @@ class Node:
         #todo
 
 
+def split_regex(regex):
+    splits = []
+    non_spliting = ['+', "*"]
+    inside_brackets = False
+    for i in range(len(regex)):
+        if inside_brackets:
+            if regex[i] == ']':
+                splits.append(i+1)
+                inside_brackets = False
+            continue
+        if i < len(regex) - 1 and regex[i+1] in non_spliting:
+            continue
+        if regex[i] == '[':
+            inside_brackets = True
+        else:
+            splits.append(i+1)
+
+    groups = []
+    beginning = 0
+    for split in splits:
+        group = regex[beginning:split]
+        groups.append(group)
+        beginning = split
+    return groups
+
+
 def create_automata(regex):
     pass
     #todo sprawdzenie czy regex jest poprawny - zawiera tylko te symbole na które się zgodziliśmy w poleceniu
     #todo pocięcie regexa na wierzchołki (transitions) ab+c -> [a, b+, c]
     #todo stworzenie całego automatu z odpowiednimi wierzchołkami
 
+if __name__ == '__main__':
+    test = ['abc', 'ab+c', 'ab[a-z][A-Z]d']
+
+    print(split_regex(test[1]))
+
+    # for t in test:
+    #     print(t, split_regex(t))
