c1861e930f95c2da1369a38638e34f9c8b8fc799
agurgul <agurgul@pgs-soft.com>
2020-07-20 20:54:15 +0200

    Add day 5 snippets

--- 504-assignment.py
+++ 504-assignment.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import *
 
 """
 
@@ -14,14 +15,25 @@ from dataclasses import dataclass
 """
 
 
-class Meal:
-    pass
+@dataclass
+class Ingredient:
+    FILE_NAME = 'ingredients.csv'
+    FROM_SPLIT_MAPPER = lambda x: Ingredient(int(x[0]), x[1])
+    TO_SPLIT_MAPPER = None
+
+    id: int
+    name: str
 
 
 @dataclass
-class Ingredient:
+class Meal:
+    FILE_NAME = 'meals.csv'
+    FROM_SPLIT_MAPPER = None
+    TO_SPLIT_MAPPER = None
+
     id: int
     name: str
+    ingredients: List[Ingredient]
 
 
 class Order:
@@ -30,3 +42,28 @@ class Order:
 
 class Cook:
     pass
+
+
+def read_ingredients():
+    return read_csv_file(Ingredient.FILE_NAME, Ingredient.FROM_SPLIT_MAPPER)
+
+
+def read_meals():
+    read_csv_file(Meal.FILE_NAME, Meal.FROM_SPLIT_MAPPER)
+
+
+def write_ingredients(ingredients: List[Ingredient]):
+    with open(Ingredient.FILE_NAME, 'w', encoding='utf-8') as f:
+        for ingredient in ingredients:
+            f.write(f'{ingredient.id};{ingredient.name}\n')
+
+
+def read_csv_file(file_name, mapper, encoding='utf-8'):
+    with open(file_name, 'r', encoding=encoding) as f:
+        for line in f:
+            yield mapper(line.strip().split(';'))
+
+
+if __name__ == '__main__':
+    for i in read_ingredients():
+        print(i)
