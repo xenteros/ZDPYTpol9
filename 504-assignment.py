e221d54e056c207982b6febb955f75b523b7c737
agurgul <agurgul@pgs-soft.com>
2020-07-21 19:05:04 +0200

    Update code

--- 504-assignment.py
+++ 504-assignment.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from typing import *
+from abc import *
 
 """
 
@@ -15,25 +16,67 @@ from typing import *
 """
 
 
-@dataclass
-class Ingredient:
-    FILE_NAME = 'ingredients.csv'
-    FROM_SPLIT_MAPPER = lambda x: Ingredient(int(x[0]), x[1])
-    TO_SPLIT_MAPPER = None
+class FileWritable(ABC):
 
+    @staticmethod
+    @abstractmethod
+    def file_name():
+        pass
+
+    @abstractmethod
+    def to_csv_string(self, sep=';'):
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def from_csv_string(csv_string, sep=';', members=None):
+        pass
+
+
+@dataclass
+class Ingredient(FileWritable):
     id: int
     name: str
 
+    @staticmethod
+    def file_name():
+        return 'ingredients.csv'
+
+    @staticmethod
+    def from_csv_string(csv_string: str, sep=';', **kwargs):
+        splited = csv_string.strip().split(sep)
+        return Ingredient(int(splited[0]), splited[1])
+
+    def to_csv_string(self, sep=';'):
+        return f'{self.id};{self.name}'
+
 
 @dataclass
-class Meal:
-    FILE_NAME = 'meals.csv'
-    FROM_SPLIT_MAPPER = None
-    TO_SPLIT_MAPPER = None
-
+class Meal(FileWritable):
     id: int
     name: str
     ingredients: List[Ingredient]
+
+    @staticmethod
+    def file_name():
+        return 'meals.csv'
+
+    @staticmethod
+    def from_csv_string(csv_string: str, members=None, sep=';'):
+        if members is None:
+            raise Exception
+        ingredients_dict = {ingredient.id: ingredient for ingredient in members}
+        splited = csv_string.split(sep)
+        return Meal(int(splited[0]), splited[1], Meal.__map_ingredients(splited[2], ingredients_dict))
+
+    def to_csv_string(self, sep=';'):
+        ingredients = ','.join(map(lambda i: str(i.id), self.ingredients))
+        return f'{self.id};{self.name};{ingredients}'
+
+    @staticmethod
+    def __map_ingredients(ids_string: str, ingredients: Dict[int, Ingredient]):
+        ids = map(int, ids_string.split(','))
+        return [ingredients[id] for id in ids]
 
 
 class Order:
@@ -45,11 +88,12 @@ class Cook:
 
 
 def read_ingredients():
-    return read_csv_file(Ingredient.FILE_NAME, Ingredient.FROM_SPLIT_MAPPER)
+    return read_csv_file(Ingredient)
 
 
 def read_meals():
-    read_csv_file(Meal.FILE_NAME, Meal.FROM_SPLIT_MAPPER)
+    ingredients = read_ingredients()
+    return list(read_csv_file(Meal, members=ingredients))
 
 
 def write_ingredients(ingredients: List[Ingredient]):
@@ -58,12 +102,26 @@ def write_ingredients(ingredients: List[Ingredient]):
             f.write(f'{ingredient.id};{ingredient.name}\n')
 
 
-def read_csv_file(file_name, mapper, encoding='utf-8'):
-    with open(file_name, 'r', encoding=encoding) as f:
+def write_csv_file(cls: Type[FileWritable], objects: List[FileWritable], encoding='utf-8', sep=';'):
+    with open(cls.file_name(), 'w', encoding=encoding) as f:
+        for object in objects:
+            f.write(object.to_csv_string(sep=sep) + '\n')
+
+
+def read_csv_file(cls: Type[FileWritable], encoding='utf-8', sep=';', members=None):
+    with open(cls.file_name(), 'r', encoding=encoding) as f:
         for line in f:
-            yield mapper(line.strip().split(';'))
+            yield cls.from_csv_string(line, sep=sep, members=members)
 
 
 if __name__ == '__main__':
-    for i in read_ingredients():
-        print(i)
+    i1 = Ingredient(1, 'pomidor')
+    i2 = Ingredient(2, 'papryka')
+
+    meal = Meal(1, 'Sałatka Szewska', [i1, i2])
+    print(meal.to_csv_string())
+    print(meal)
+    write_csv_file(Meal, [meal])
+    write_csv_file(Ingredient, [i1, i2])
+    meals = read_meals()
+    print(meals)
